5eafec8db067a1f40c8d94a6d6f303ee0547fb8a
Hyomin Cha <113211090+Chagong725@users.noreply.github.com>
2023-06-01 17:26:06 -0600

    Create stack.py

---
+++ 4.29 Project 5: Stacks/stack.py
@@ -0,0 +1,25 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        raise IndexError("Stack is empty.")
+
+    def top(self):
+        if not self.is_empty():
+            return self.items[-1]
+        raise IndexError("Stack is empty.")
+
+    def size(self):
+        return len(self.items)
+
+    def clear(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
