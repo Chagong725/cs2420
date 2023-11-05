132b9baa29c11ff4859a9237ba052ec68c4e8abc
Hyomin Cha <113211090+Chagong725@users.noreply.github.com>
2023-11-05 14:53:56 -0600

    Update bst.py

--- 5.14 Project 6: Binary Search Tree/bst.py
+++ 5.14 Project 6: Binary Search Tree/bst.py
@@ -15,6 +15,7 @@ class BST:
         if node is None:
             return 0
         else:
+            # +1은 현재 노드
             return 1 + self._size(node.left) + self._size(node.right)
 
     def is_empty(self):
@@ -33,8 +34,11 @@ class BST:
         self.root = self._add(self.root, item)
 
     def _add(self, node, item):
+        # 노드가 0이거나 적절한 위치에 도달
         if node is None:
             return Node(item)
+        '''BST에서는 각 노드의 왼쪽 서브트리에 있는 모든 노드의 값이 해당 노드의 값보다 작아야 하며, 
+        오른쪽 서브트리에 있는 모든 노드의 값이 해당 노드의 값보다 커야 합니다.'''
         elif item < node.data:
             node.left = self._add(node.left, item)
         elif item > node.data:
@@ -45,12 +49,14 @@ class BST:
         self.root = self._remove(self.root, item)
 
     def _remove(self, node, item):
+        # 노드가 비었거나 특정 아이템 못찾으면
         if node is None:
             return None
         if item < node.data:
             node.left = self._remove(node.left, item)
         elif item > node.data:
             node.right = self._remove(node.right, item)
+        # 제거 아이템이 현재 노드 데이터와 같은 경우
         else:
             if node.left is None and node.right is None:
                 return None
@@ -58,12 +64,17 @@ class BST:
                 return node.right
             elif node.right is None:
                 return node.left
+            # 두개 자식 모두 있음
             else:
+                # 오른쪽 서브트리에서 가장 작으거 찾아
                 temp = self._find_min(node.right)
+                # 현제 노드 데이터를 가장 작은거로 교체
                 node.data = temp.data
+                # 그거를 오른쪽에서 제거
                 node.right = self._remove(node.right, temp.data)
         return node
 
+    # 주어진 노드 서브트리에서 가장 작은 노드 찾기
     def _find_min(self, node):
         while node.left is not None:
             node = node.left
@@ -86,6 +97,7 @@ class BST:
         return self._inorder(self.root)
 
     def _inorder(self, node):
+        # 트리가 비었거나 방문했으면
         if node is None:
             return []
         else:
